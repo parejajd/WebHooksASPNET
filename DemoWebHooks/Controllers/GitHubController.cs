e34941884525b67ce77ee97ea0701186a01510c4
Juan David Pareja Soto <parejajd@outlook.com>
2021-11-19 18:10:59 -0500

    Implementation

---
+++ DemoWebHooks/Controllers/GitHubController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebHooks;
+using Newtonsoft.Json.Linq;
+
+namespace DemoWebHooks.Controllers
+{
+    public class GitHubController : ControllerBase
+    {
+        private readonly ILogger<GitHubController> _logger;
+
+        public GitHubController(ILogger<GitHubController> logger)
+        {
+            _logger = logger;
+        }
+
+        [GitHubWebHook(EventName = "push", Id = "It")]
+        public IActionResult HandlerForItsPushes(string[] events, JObject data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _logger.LogInformation("push it", events);
+            return Ok();
+        }
+
+        [GitHubWebHook(Id = "It")]
+        public IActionResult HandlerForIt(string[] events, JObject data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _logger.LogInformation("It", events);
+            return Ok();
+        }
+
+        [GitHubWebHook(EventName = "push")]
+        public IActionResult HandlerForPush(string id, JObject data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _logger.LogInformation("push", id);
+            return Ok();
+        }
+
+        [GitHubWebHook]
+        public IActionResult GitHubHandler(string id, string @event, JObject data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _logger.LogInformation("GitHubHandler", id,@event);
+            return Ok();
+        }
+
+        [GeneralWebHook]
+        public IActionResult FallbackHandler(string receiverName, string id, string eventName)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _logger.LogInformation("FallbackHandler", receiverName, id, eventName);
+            return Ok();
+        }
+    }
+}
